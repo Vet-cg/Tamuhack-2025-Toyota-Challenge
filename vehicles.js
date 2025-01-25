2e309d6b5dcb15f405c3b5b5c0913797e295ec3c
Noah Zhou <component_caddy033@silomails.com>
2025-01-25 15:13:57 -0600

    update vehicle.js

---
+++ vehicles.js
@@ -0,0 +1,7 @@
+const rav4_dict = {
+    "apple": "A fruit that is typically red, green, or yellow.",
+    "banana": "A long, yellow fruit.",
+    "car": "A vehicle used for transportation.",
+    "dog": "A domesticated carnivorous mammal."
+  };
+  
