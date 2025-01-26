ef3b46def86f2f25a9b32262fa5e87f6abf06db5
Noah Zhou <component_caddy033@silomails.com>
2025-01-25 19:41:18 -0600

    4runner, b4zxk, highlander

---
+++ bZ4XDict.js
@@ -0,0 +1,58 @@
+const EVbZFourXXLE_dict = {
+    price: "$37,070",
+    milage: "252 Miles",
+    seats: "5",
+    features: [
+        "Toyota Safety Sense™ 3.0 (TSS 3.0)",
+        "Fixed panoramic roof with power sunshade",
+        "SofTex® /fabric-trimmed seats with ample leg room",
+        "Wireless Android Auto™ and Apple CarPlay® compatibility",
+        "3-year Remote Connect and 3-year Drive Connect trials. 4G network dependent."
+    ],
+    Drivetrain: [
+        "Front-Wheel Drive (FWD)",
+        "Available All-Wheel Drive (AWD) with X-MODE"
+    ],
+    Performance: "AC synchronous electric motor(s), 201 total system horsepower (FWD), 214 total combined system horsepower (AWD)",
+    targetAudience: "The Toyota bZ4X XLE targets environmentally-minded drivers who seek an affordable, tech-rich electric SUV with practical features and a stylish design."
+}
+const EVbZFourXNighshade_dict = {
+    price: "$40,420",
+    milage: "222 Miles",
+    seats: "5",
+    features: [
+        "Toyota Safety Sense™ 3.0 (TSS 3.0)",
+        "Fixed panoramic roof with power sunshade",
+        "SofTex® /fabric-trimmed seats with ample leg room",
+        "Wireless Android Auto™ and Apple CarPlay® compatibility",
+        "3-year Remote Connect and 3-year Drive Connect trials. 4G network dependent."
+    ],
+    Drivetrain: [
+        "All-Wheel Drive (AWD) with X-MODE"
+    ],
+    Performance: [
+        "AC synchronous electric motor(s); 214 total combined system horsepower (AWD)"
+    ],
+    targetAudience: "The Toyota bZ4X Nightshade targets style-conscious, eco-aware drivers who want a distinctive, blacked-out design paired with the benefits of an electric vehicle."
+}
+const EVbZFourXLimited_dict = {
+    price: "$41,800",
+    milage: "236 Miles",
+    seats: "5",
+    features: [
+        "Toyota Safety Sense™ 3.0 (TSS 3.0)",
+        "Fixed panoramic roof with power sunshade",
+        "SofTex® /fabric-trimmed seats with ample leg room",
+        "Wireless Android Auto™ and Apple CarPlay® compatibility",
+        "3-year Remote Connect and 3-year Drive Connect trials. 4G network dependent."
+    ],
+    Drivetrain: [
+        "Front-Wheel Drive (FWD)",
+        "Available All-Wheel Drive (AWD) with X-MODE"
+    ],
+    Performance: [
+        "AC synchronous electric motors(s), 201 total system horsepower (FWD), 214 total combined system horsepower (AWD)"
+
+    ],
+    targetAudience: "The Toyota bZ4X Limited targets premium-minded, eco-conscious drivers seeking a high-end electric SUV with advanced features, luxury touches, and enhanced performance."
+}
