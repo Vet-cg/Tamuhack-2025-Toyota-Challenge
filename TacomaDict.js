837937c9cdf2f397bf4d23d5239b7f86180b5dce
kream-cheese <haydenfr33man@tamu.edu>
2025-01-25 21:56:40 -0600

    Create TacomaDict.js

---
+++ TacomaDict.js
@@ -0,0 +1,117 @@
+const TacomaSR =  {
+      price: "$31,590",
+      mileage: "20/26",
+      seats: "2",
+      features: [
+        "Smart Key System",
+        "17-in. alloy wheels (Double Cab Only)",
+        "8-in. audio multimedia display with wireless Apple CarPlay and Android Audio compability"
+      ],
+      Drivetrain: [
+        "8-speed Automatic Transmission",
+        "6-speed Manual Transmission (Double Cab only)",
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 228 hp; 243 lb.-ft. of torque (Automatic Transmission)",
+        "i-FORCE 2.4L turbocharged engine: 270 hp; 310 lb.-ft. of torque (Manual Transmission)"
+      ],
+      targetAudience: "Budget-conscious individuals seeking a reliable truck for light-duty tasks."
+    }
+   const TacomaSR5 = {
+      price: "$36,220",
+      mileage: "21/26",
+      seats: "2",
+      features: [
+        "Coil-spring multi-link rear suspension (Double Cab only)",
+        "8-in. audio multimedia display with wireless Apple CarPlay® * and Android Auto™ * compatibility",
+        "Leather-wrapped steering wheel",
+        "17-in. alloy wheels"
+      ],
+      Drivetrain: [
+        "i-FORCE 2.4L turbocharged engine",
+        "8-speed Automatic Transmission"
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 278 hp; 317 lb.-ft. of torque"
+      ],
+      targetAudience: "Drivers who want enhanced comfort and convenience for daily use."
+    }
+    const TacomaPrerunner = {
+      price: "$38,120",
+      mileage: "20/24",
+      seats: "2",
+      features: [
+        "Electronically controlled locking rear differential",
+        "32-in. BFGoodrich® all-terrain tires",
+        "Front suspension lift"
+      ],
+      Drivetrain: [
+        "Rear-Wheel Drive (RWD) with Automatic Limited-Slip Differential (Auto LSD) *",
+        "8-speed Automatic Transmission"
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 278 hp; 317 lb.-ft. of torque"
+      ],
+      targetAudience: "Adventurous individuals seeking an affordable, rugged, and reliable midsize truck with off-road styling for light to moderate outdoor activities."
+    }
+    const TacomaTRDSport = {
+      price: "$39,400",
+      mileage: "20/24",
+      seats: "5",
+      features: [
+        "TRD sport-tuned, twin-tube shocks",
+        "18-in. TRD Sport alloy wheels",
+        "12.3-in. digital gauge cluster",
+      ],
+      Drivetrain: [
+        "8-speed Automatic Transmission",
+        "6-speed Manual Transmission",
+        "4WDemand part-time 4-Wheel Drive",
+        "Rear-Wheel Drive (RWD) with Automatic Limited-Slip Differential (Auto LSD) *"
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 278 hp; 317 lb.-ft. of torque (Automatic Transmission)",
+        "i-FORCE 2.4L turbocharged engine: 270 hp; 310 lb.-ft. of torque (Manual Transmission)."
+      ],
+      targetAudience: "Urban drivers seeking a stylish truck with sporty handling."
+    }
+    const TRD_OffRoad = {
+      price: "$41,800",
+      mileage: "18/23",
+      seats: "5",
+      features: [
+        "Electronically controlled locking rear differential",
+        "Available Stabilizer Disconnect Mechanism (SDM)",
+        "Available Multi-Terrain Select with Crawl Control"
+      ],
+      Drivetrain: [
+        "8-speed Automatic Transmission",
+        "6-speed Manual Transmission",
+        "4WDemand part-time 4-Wheel Drive"
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 278 hp; 317 lb.-ft. of torque (Automatic Transmission)",
+        "i-FORCE 2.4L turbocharged engine: 270 hp; 310 lb.-ft. of torque (Manual Transmission)"
+      ],
+      targetAudience: "Adventure enthusiasts who need a capable off-road vehicle."
+    }
+    const TacomaLimited = {
+      price: "$52,555",
+      mileage: "20/23",
+      seats: "5",
+      features: [
+        "Adaptive Variable Suspension (AVS)",
+        "Power open-and-close tailgate * and power-extending running boards",
+        "SofTex®-trimmed interior with walnut burl-wood accents"
+      ],
+      Drivetrain: [
+        "8-speed Automatic Transmission",
+        "4WDemand part-time 4-Wheel Drive"
+      ],
+      Performance: [
+        "i-FORCE 2.4L turbocharged engine: 278 hp; 317 lb.-ft. of torque (Automatic Transmission)"
+      ],
+      targetAudience: "Drivers who prioritize luxury and premium features in a midsize truck."
+    }
+    ;
+  
