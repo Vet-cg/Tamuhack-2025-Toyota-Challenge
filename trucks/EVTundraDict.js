d77d405a1532eb0537f284735727da3a72a21a2e
Noah Zhou <component_caddy033@silomails.com>
2025-01-25 21:00:44 -0600

    formatting things

---
+++ trucks/EVTundraDict.js
@@ -0,0 +1,104 @@
+const TundraLimitediFORCEMAX =  {
+      price: "$58,005",
+      mileage: "20/24",
+      seats: "5",
+      features: [
+        "20-in. alloy wheels",
+        "14-in. Toyota Audio Multimedia touchscreen",
+        "SofTex®-trimmed * 8-way power-adjustable front seats",
+        "Blind Spot Monitor (BSM) * with Rear Cross-Traffic Alert (RCTA) * and Trailer Merge Warning",
+        "Available TRD Off-Road Package",
+        "Toyota Safety Sense™ 2.5 (TSS 2.5) *"
+      ],
+      Drivetrain: [
+        "i-FORCE MAX Twin-Turbo V6 Hybrid",
+        "10-speed Electronically Controlled automatic Transmission with intelligence (ECT-i)",
+        "Rear-Wheel Drive or 4-Wheel Drive",
+      ],
+      Performance: [
+        "V6 hybrid: 437 hp; 583 lb.-ft."
+      ],
+      targetAudience: "Buyers seeking a powerful and fuel-efficient full-size truck for heavy-duty tasks, towing, and premium features."
+    }
+    const TundraPlatinumiFORCEMAX = {
+        priceRange: "$67,585",
+        mileage: "20/24",
+        seats: "5",
+        features: [
+            "20-in. dark-gray-painted alloy wheels",
+            "Power tilt/slide panoramic roof with power sunshade",
+            "Leather-trimmed 10-way power-adjustable front seats",
+            "12.3-in. digital meter with selectable gauge display screens",
+            "Available Adaptive Variable Suspension (AVS) and load-leveling rear air suspension",
+            "Toyota Safety Sense™ 2.5 (TSS 2.5) *"
+        ],
+        Drivetrain: [
+           "i-FORCE MAX Twin-Turbo V6 Hybrid",
+            "10-speed Electronically Controlled automatic Transmission with intelligence (ECT-i)",
+            "Rear-Wheel Drive or 4-Wheel Drive"
+        ],
+        Performance: [
+            "V6 hybrid: 437 hp; 583 lb.-ft."
+        ],
+        targetAudience: "Luxury-seeking, eco-conscious buyers who need a powerful, tech-rich truck for towing, outdoor adventures, and everyday comfort."
+    }
+    const onesevenninefourEditioniFORCEMAX = {
+        price: "$68,285",
+        mileage: "20/24",
+        seats: "5",
+        features: [
+            "20-in. machine-finished alloy wheels",
+            "Chrome-finsihed grille and exterior accents",
+            "Saddle Tan interior with American walnut wood-grain trim",
+            "Trailer Backup Guide with Straight Path Assist (SPA)",
+            "Available TRD Off-Road Package",
+            "Toyota Safety Sense™ 2.5 (TSS 2.5) *"
+        ],
+        Drivetrain: [
+            "i-FORCE MAX Twin-Turbo V6 Hybrid",
+            "10-speed Electronically Controlled automatic Transmission with intelligence (ECT-i)",
+            "Rear-Wheel Drive or 4-Wheel Drive"
+        ],
+        Performance: [
+            "V6 hybrid: 437 hp; 583 lb.-ft."
+        ],
+        targetAudience: " buyers who value rugged performance, premium luxury features, and a uniquely styled truck with a heritage-inspired design for both work and leisure"
+    }
+    const TundraTRDPro = {
+        price: "$72,510",
+        mileage: "18/20",
+        seats: "5",
+        features: [
+            "18-in. BBS forged-aluminum wheels with Falken WILDPEAK All-Terrain tires",
+            "TRD Pro off-road suspension with FOX shocks and 1.1-in front lift",
+            "Heritage-inspired 'TOYOTA' grille with LED light bar and marker tires",
+            "12-speaker JBL Premium Audio",
+            "Multi-Terrain Select (MTS) with Crawl Control (CRAWL) *",
+            "Electronically controlled locking rear differential",
+            "Toyota Safety Sense™ 2.5 (TSS 2.5) *"
+        ],
+        Drivetrain: [
+            "i-FORCE MAX Twin-Turbo V6 Hybrid",
+            "4-Wheel Drive"
+        ],
+        Performance: [
+            "437 hp; 583 lb.-ft."
+        ],
+        targetAudience: "hardcore off-road enthusiasts seeking unmatched performance, durability, and advanced off-road technology for extreme terrains."
+    }
+    const TundraCapstone = {
+        priceRange: "$80,725",
+        mileage: "19/22",
+        seats: "5",
+        features: [
+            "22-in. chrome- and machined-finish alloy wheels",
+            "Semi-aniline leather-trimmed 10-way power-adjustable front seats",
+            "Two-tone white-and-black interior with dark American walnut wood-grain trim",
+            "10-in. color Head-Up Display (HUD)",
+            "Power running boards and BedStep® ",
+            "Available Adaptive Variable Suspension (AVS)",
+            "Toyota Safety Sense™ 2.5 (TSS 2.5) *"
+        ],
+        targetAudience: "Buyers seeking the pinnacle of luxury, advanced technology, and hybrid performance in a full-size truck"
+    };
+  
