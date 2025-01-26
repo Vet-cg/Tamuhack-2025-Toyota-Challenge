3849cf63343b06507b1f1e34f185e9cb40ce2e6b
Vet-cg <cbpcbphillips@gmail.com>
2025-01-25 18:43:08 -0600

    background and buttons
    some generic background photos uploaded as well as working on quiz, also got rid of some redundant code

--- quiz.js
+++ quiz.js
@@ -1,18 +1,10 @@
 const startBtn = document.getElementById("startBtn");
 const intro = document.getElementById("intro");
+const controls = document.getElementById("controls");
 const question1 = document.getElementById("question1");
 
-console.log(startBtn);
-console.log(intro);
-console.log(question1);
-
-
-const startBtnPressed = async(e) => {
-    e.preventDefault();
-    intro.classList.add("hide");
-    question1.classList.remove("hide");
-}
 startBtn.addEventListener("click", function() {
     intro.classList.add("hide");
     question1.classList.remove("hide");
 });
+
